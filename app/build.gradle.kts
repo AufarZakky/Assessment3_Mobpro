d7d300ff14808c5e562db631d96c6260077410f8
Aufar <aufarzakky@gmail.com>
2024-05-30 14:37:58 +0700

    3.2.Membuat fitur login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
